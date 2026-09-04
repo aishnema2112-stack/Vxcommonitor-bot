b2eceaf46d5154dea0acdf69b7eb7b5fc1318581
aishnema2112-stack <aishnema2112@gmail.com>
2026-09-04 06:56:17 +0530

    Update main.py

--- main.py
+++ main.py
@@ -11,8 +11,6 @@ import telebot
 from telebot import types
 from datetime import datetime, timezone, timedelta
 import psycopg2
-from psycopg2.rows import dict_row
-
 # Force unbuffered stdout for Render logs
 sys.stdout.reconfigure(line_buffering=True)
 
